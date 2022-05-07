20e0771823e4c34e30d34da2569f7c207b2a7f35
suicedek <handsomefox.hf@gmail.com>
2022-05-07 22:11:55 +0300

    refactor

--- main.go
+++ main.go
@@ -2,30 +2,14 @@ package main
 
 import (
 	"fmt"
-	"gowarp/pkg/keygen"
+	"gowarp/pkg/warpgen"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func warp(w http.ResponseWriter, r *http.Request) {
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		http.Error(w, "Server does not support Flusher!", http.StatusInternalServerError)
-		return
-	}
-
-	ua := r.UserAgent()
-
-	if strings.Contains(ua, "Firefox/") {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	} else {
-		w.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
-	}
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	if err := keygen.Generate(w, flusher); err != nil {
+	if err := warpgen.Generate(w, r); err != nil {
 		fmt.Fprintln(w, err)
 		return
 	}
--- pkg/warpgen/warpgen.go
+++ pkg/warpgen/warpgen.go
@@ -1,4 +1,4 @@
-package keygen
+package warpgen
 
 import (
 	"bytes"
@@ -8,17 +8,14 @@ import (
 	"gowarp/pkg/progressbar"
 	"math/rand"
 	"net/http"
+	"strings"
 	"time"
 )
 
-func init() {
-	client = createClient()
-}
-
 var client *http.Client
 
-func createClient() *http.Client {
-	return &http.Client{
+func init() {
+	client = &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
 				MinVersion: tls.VersionTLS12,
@@ -36,8 +33,17 @@ func createClient() *http.Client {
 const (
 	cfClientVersion = "a-6.11-2223"
 	userAgent       = "okhttp/3.12.1"
+	host            = "api.cloudflareclient.com"
 	baseURL         = "https://api.cloudflareclient.com/v0a2223"
 )
+
+var headers = http.Header{
+	"Host":              []string{host},
+	"CF-Client-Version": []string{cfClientVersion},
+	"User-Agent":        []string{userAgent},
+	"Connection":        []string{"Keep-Alive"},
+	"Accept-Encoding":   []string{"gzip"},
+}
 
 var keys = []string{
 	"47d58Hqv-ueR37x50-db3l70n2",
@@ -52,18 +58,18 @@ var keys = []string{
 	"ofQ759g4-9628GDiy-6i58VGa2",
 }
 
-type result struct {
+type finalAccount struct {
 	Type     string      `json:"account_type"`
 	RefCount json.Number `json:"referral_count"`
 	License  string      `json:"license"`
 }
 
-type createdAccounts struct {
-	First  *account
-	Second *account
+type registeredAccounts struct {
+	First  *acc
+	Second *acc
 }
 
-type account struct {
+type acc struct {
 	Id      string  `json:"id"`
 	Account license `json:"account"`
 	Token   string  `json:"token"`
@@ -73,7 +79,18 @@ type license struct {
 	License string `json:"license"`
 }
 
-func Generate(w http.ResponseWriter, flusher http.Flusher) error {
+func Generate(w http.ResponseWriter, r *http.Request) error {
+	flusher, _ := w.(http.Flusher)
+
+	ua := r.UserAgent()
+	if strings.Contains(ua, "Firefox/") {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	} else {
+		w.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
+	}
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
 	pb := progressbar.New(w, flusher)
 	pb.Update(10)
 
@@ -126,142 +143,157 @@ func Generate(w http.ResponseWriter, flusher http.Flusher) error {
 	return nil
 }
 
-func createAccounts() (*createdAccounts, error) {
-	acc1, err := register()
+func createAccounts() (*registeredAccounts, error) {
+	first, err := register()
 	if err != nil {
 		return nil, err
 	}
 
-	acc2, err := register()
+	second, err := register()
 	if err != nil {
 		return nil, err
 	}
 
-	accounts := &createdAccounts{
-		First:  acc1,
-		Second: acc2,
+	accounts := &registeredAccounts{
+		First:  first,
+		Second: second,
 	}
 
 	return accounts, nil
 }
 
-func register() (*account, error) {
-	req, err := http.NewRequest("POST", baseURL+"/reg", nil)
-	req.Header.Set("CF-Client-Version", cfClientVersion)
-	req.Header.Set("User-Agent", userAgent)
+func register() (*acc, error) {
+	request, err := http.NewRequest("POST", baseURL+"/reg", nil)
+	request.Header = headers
 
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := client.Do(req)
+	response, err := client.Do(request)
 	if err != nil {
 		return nil, err
 	}
 
-	var reg account
-	err = json.NewDecoder(resp.Body).Decode(&reg)
+	var registered acc
+	err = json.NewDecoder(response.Body).Decode(&registered)
 
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
 
-	return &reg, err
+	return &registered, err
 }
 
-func addReferrer(accounts *createdAccounts) error {
+func addReferrer(accounts *registeredAccounts) error {
 	payload, _ := json.Marshal(map[string]interface{}{
 		"referrer": accounts.Second.Id,
 	})
 
 	url := baseURL + fmt.Sprintf("/reg/%s", accounts.First.Id)
-	patchRequest, err := http.NewRequest("PATCH", url, bytes.NewBuffer(payload))
+	request, err := http.NewRequest("PATCH", url, bytes.NewBuffer(payload))
 
 	if err != nil {
 		return err
 	}
 
-	patchRequest.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	patchRequest.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
+	request.Header = headers
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
 
-	_, err = client.Do(patchRequest)
+	_, err = client.Do(request)
 	return err
 }
 
-func deleteSecondAccount(accounts *createdAccounts) error {
+func deleteSecondAccount(accounts *registeredAccounts) error {
 	url := baseURL + fmt.Sprintf("/reg/%s", accounts.Second.Id)
-	deleteRequest, err := http.NewRequest("DELETE", url, nil)
+	request, err := http.NewRequest("DELETE", url, nil)
+
 	if err != nil {
 		return err
 	}
 
-	deleteRequest.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.Second.Token))
-	_, err = client.Do(deleteRequest)
+	request.Header = headers
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.Second.Token))
+
+	_, err = client.Do(request)
 	return err
 }
 
-func setFirstAccountKey(accounts *createdAccounts) error {
+func setFirstAccountKey(accounts *registeredAccounts) error {
 	key := keys[rand.Intn(len(keys))]
+
 	payload, _ := json.Marshal(map[string]interface{}{
 		"license": key,
 	})
+
 	url := baseURL + fmt.Sprintf("/reg/%s/account", accounts.First.Id)
-	putRequest, err := http.NewRequest("PUT", url, bytes.NewBuffer(payload))
+	request, err := http.NewRequest("PUT", url, bytes.NewBuffer(payload))
 	if err != nil {
 		return err
 	}
 
-	putRequest.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	putRequest.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
-	_, err = client.Do(putRequest)
+	request.Header = headers
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
+
+	_, err = client.Do(request)
 	return err
 }
 
-func setFirstAccountLicense(accounts *createdAccounts) error {
+func setFirstAccountLicense(accounts *registeredAccounts) error {
 	payload, _ := json.Marshal(map[string]interface{}{
 		"license": accounts.First.Account.License,
 	})
+
 	url := baseURL + fmt.Sprintf("/reg/%s/account", accounts.First.Id)
-	putRequest, err := http.NewRequest("PUT", url, bytes.NewBuffer(payload))
+	request, err := http.NewRequest("PUT", url, bytes.NewBuffer(payload))
 	if err != nil {
 		return err
 	}
+	request.Header = headers
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
 
-	putRequest.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	putRequest.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
-	_, err = client.Do(putRequest)
+	_, err = client.Do(request)
 	return err
 }
 
-func getLicenseInformation(accounts *createdAccounts) (*result, error) {
+func getLicenseInformation(accounts *registeredAccounts) (*finalAccount, error) {
 	url := baseURL + fmt.Sprintf("/reg/%s/account", accounts.First.Id)
-	getRequest, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	getRequest.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
-
-	resp, err := client.Do(getRequest)
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	var result result
-	err = json.NewDecoder(resp.Body).Decode(&result)
+	request.Header = headers
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
+
+	response, err := client.Do(request)
 	if err != nil {
 		return nil, err
 	}
+
+	var result finalAccount
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+
 	return &result, nil
 }
 
-func deleteAccount(accounts *createdAccounts) error {
+func deleteAccount(accounts *registeredAccounts) error {
 	url := baseURL + fmt.Sprintf("/reg/%s", accounts.First.Id)
-	deleteRequest, err := http.NewRequest("DELETE", url, nil)
+	request, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
 		return err
 	}
-	deleteRequest.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
-	_, err = client.Do(deleteRequest)
+
+	request.Header = headers
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accounts.First.Token))
+
+	_, err = client.Do(request)
 	return err
 }
