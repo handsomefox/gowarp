0cb4097a5a6ebf460bbdd24f8394ff4a859c890c
suicedek <handsomefox.hf@gmail.com>
2022-05-24 13:58:00 +0300

    Move configuration to separate package

---
+++ pkg/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+const (
+	CfClientVersion = "a-6.13-2355"
+	UserAgent       = "okhttp/3.12.1"
+	Host            = "api.cloudflareclient.com"
+	BaseURL         = "https://api.cloudflareclient.com/v0a2355"
+)
+
+var Keys = []string{
+	"10FbK2D8-VK3hZ675-h2Gx315V",
+	"9D2aP6y1-ay218hX0-i0k9g8L1",
+	"85Ry64SN-i8kBN514-4drF821q",
+	"5Cn76eB4-r8P20v6w-9Ve0u65R",
+	"0f61Z7tR-8t34mE1S-g2u846kC",
+	"80cqt79O-95Y40QCm-n9u3Yt45",
+	"0sMV37f4-61C5Q2Uv-82B5iYs7",
+	"iH9TD408-49gsX50J-3B8fGx67",
+	"1DIq085C-1g6MuL58-A8D2WM31",
+	"6Tyj459c-9if2z5u3-9U3FJv52",
+}
