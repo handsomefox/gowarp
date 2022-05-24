2f66ba983ebe6f1d00bd4b04cdfd035599ca5a04
suicedek <handsomefox.hf@gmail.com>
2022-05-24 14:03:28 +0300

    Better error messages

--- pkg/warpgen/errors.go
+++ pkg/warpgen/errors.go
@@ -3,12 +3,12 @@ package warpgen
 import "errors"
 
 var (
-	ErrCreateRequest   = errors.New("error creating a request")
-	ErrRegisterAccount = errors.New("error when registering an account, maybe rate limited by cf")
+	ErrCreateRequest   = errors.New("error creating request")
+	ErrRegisterAccount = errors.New("error when registering account, maybe rate limited by cf")
 	ErrDecodeResponse  = errors.New("error decoding response")
-	ErrSetKey          = errors.New("error when setting account key")
-	ErrDeleteAccount   = errors.New("error when deleting account")
-	ErrAddReferrer     = errors.New("error when adding referrer")
-	ErrFetchAccData    = errors.New("error when fetching account data")
-	ErrDecodeAccData   = errors.New("error when deconding account data from json")
+	ErrSetKey          = errors.New("error setting account key")
+	ErrDeleteAccount   = errors.New("error deleting account")
+	ErrAddReferrer     = errors.New("error adding referrer")
+	ErrFetchAccData    = errors.New("error fetching account data")
+	ErrDecodeAccData   = errors.New("error decoding account data from json")
 )
