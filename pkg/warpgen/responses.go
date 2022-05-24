309f81e33b27296d4ce9d1c470366337d065343f
suicedek <handsomefox.hf@gmail.com>
2022-05-24 13:57:47 +0300

    Move responses to a file, add functions to account

---
+++ pkg/warpgen/responses.go
@@ -0,0 +1,100 @@
+package warpgen
+
+import (
+	"bytes"
+	"encoding/json"
+	"gowarp/pkg/config"
+	"net/http"
+)
+
+type accountData struct {
+	Type     string      `json:"account_type"`
+	RefCount json.Number `json:"referral_count"`
+	License  string      `json:"license"`
+}
+
+type account struct {
+	Id      string  `json:"id"`
+	Account license `json:"account"`
+	Token   string  `json:"token"`
+}
+
+type license struct {
+	License string `json:"license"`
+}
+
+func (acc *account) addReferrer(second *account) error {
+	payload, _ := json.Marshal(map[string]string{
+		"referrer": second.Id,
+	})
+	request, err := http.NewRequest("PATCH", config.BaseURL+"/reg/"+acc.Id, bytes.NewBuffer(payload))
+	if err != nil {
+		return ErrCreateRequest
+	}
+	setCommonHeaders(request)
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("Authorization", "Bearer "+acc.Token)
+
+	_, err = client.Do(request)
+	if err != nil {
+		return ErrAddReferrer
+	}
+	return nil
+}
+
+func (acc *account) removeDevice() error {
+	request, err := http.NewRequest("DELETE", config.BaseURL+"/reg/"+acc.Id, nil)
+	if err != nil {
+		return ErrCreateRequest
+	}
+	setCommonHeaders(request)
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("Authorization", "Bearer "+acc.Token)
+
+	_, err = client.Do(request)
+	if err != nil {
+		return ErrDeleteAccount
+	}
+	return nil
+}
+
+func (acc *account) setKey(key string) error {
+	payload, _ := json.Marshal(map[string]string{
+		"license": key,
+	})
+	request, err := http.NewRequest("PUT", config.BaseURL+"/reg/"+acc.Id+"/account", bytes.NewBuffer(payload))
+	setCommonHeaders(request)
+	if err != nil {
+		return ErrCreateRequest
+	}
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("Authorization", "Bearer "+acc.Token)
+
+	_, err = client.Do(request)
+	if err != nil {
+		return ErrSetKey
+	}
+	return nil
+}
+
+func (acc *account) fetchAccountData() (*accountData, error) {
+	request, err := http.NewRequest("GET", config.BaseURL+"/reg/"+acc.Id+"/account", nil)
+	if err != nil {
+		return nil, ErrCreateRequest
+	}
+	setCommonHeaders(request)
+	request.Header.Set("Authorization", "Bearer "+acc.Token)
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, ErrFetchAccData
+	}
+	defer response.Body.Close()
+
+	result := accountData{}
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		return nil, ErrDecodeAccData
+	}
+	return &result, nil
+}
