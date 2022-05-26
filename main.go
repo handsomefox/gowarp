fd64874efb4962e5fdb00aa4e80c10d182d10d28
suicedek <handsomefox.hf@gmail.com>
2022-05-26 15:38:22 +0300

    Better IP reading

--- main.go
+++ main.go
@@ -92,11 +92,22 @@ func main() {
 
 func limitMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		limiter := limiter.GetLimiter(r.RemoteAddr)
+		limiter := limiter.GetLimiter(ReadUserIP(r))
 		if !limiter.Allow() {
 			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
 			return
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func ReadUserIP(r *http.Request) string {
+	IPAddress := r.Header.Get("X-Real-Ip")
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if IPAddress == "" {
+		IPAddress = r.RemoteAddr
+	}
+	return IPAddress
 }
