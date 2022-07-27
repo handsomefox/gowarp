f2a19dc68755ef6708067d84ceb4a9cea19dae06
suicedek <handsomefox.hf@gmail.com>
2022-07-27 16:13:12 +0300

    remove ratelimiting

--- main.go
+++ main.go
@@ -5,19 +5,16 @@ import (
 	"net/http"
 	"os"
 
-	"gowarp/ratelimiter"
 	"gowarp/warp"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	limiter := ratelimiter.New(1, 4)
-	limiterMiddleware := ratelimiter.NewMiddleware(limiter)
 	warpHandle := warp.New()
 
-	mux.Handle("/", limiterMiddleware(warpHandle))
-	mux.Handle("/config/update", limiterMiddleware(warpHandle.GetConfig()))
+	mux.Handle("/", warpHandle)
+	mux.Handle("/config/update", warpHandle.GetConfig())
 
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), mux))
 }
