3466a126aa5cba898b45be046ce5ddba8e0ca61c
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-09-13 20:24:03 +0300

    Add rate limiting

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gowarp
 
 go 1.18
+
+require github.com/sethvargo/go-limiter v0.7.2
--- main.go
+++ main.go
@@ -4,10 +4,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"gowarp/pkg/server"
+
+	"github.com/sethvargo/go-limiter/httplimit"
+	"github.com/sethvargo/go-limiter/memorystore"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), server.New()))
+	store, err := memorystore.New(&memorystore.Config{
+		Tokens:   10,
+		Interval: time.Minute,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	middleware, err := httplimit.NewMiddleware(store, httplimit.IPKeyFunc())
+	if err != nil {
+		panic(err)
+	}
+
+	log.Fatal(http.ListenAndServe(":8080"+os.Getenv("PORT"), middleware.Handle(server.New())))
 }
