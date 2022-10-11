b6af93ac27d179e97d99e31f14a32746ecc294ad
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-10-11 15:36:49 +0300

    main: don't use http.ListenAndServe
    It does not have default timeout for the requests.

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"gowarp/pkg/server"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), server.New()))
+	sh, err := server.NewHandler()
+	if err != nil {
+		panic(err)
+	}
+
+	srv := &http.Server{
+		Addr:              ":" + os.Getenv("PORT"),
+		Handler:           sh,
+		ReadTimeout:       5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
