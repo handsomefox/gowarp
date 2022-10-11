49717a48c617c6ad5a55f3bfd244451a5b3e31fd
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-10-11 15:34:36 +0300

    pkg/warp: improve error handling

--- pkg/warp/account.go
+++ pkg/warp/account.go
@@ -49,95 +49,96 @@ type License struct {
 }
 
 func (acc *Account) addReferrer(client *http.Client, cdata *ConfigData, other *Account) error {
-	payload, _ := json.Marshal(map[string]string{
+	payload, err := json.Marshal(map[string]string{
 		"referrer": other.ID,
 	})
-
-	req, err := http.NewRequest("PATCH", cdata.BaseURL+"/reg/"+acc.ID, bytes.NewBuffer(payload))
 	if err != nil {
-		return ErrCreateRequest
+		return fmt.Errorf("%s: %w", "error marshalling account referrer", err)
+	}
+
+	req, err := http.NewRequestWithContext(context.TODO(), http.MethodPatch,
+		cdata.BaseURL+"/reg/"+acc.ID, bytes.NewBuffer(payload))
+	if err != nil {
+		return fmt.Errorf("%s: %w", "error creating request with account referrer", err)
 	}
 
 	req = setCommonHeaders(cdata, req)
-
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	req.Header.Set("Authorization", "Bearer "+acc.Token)
 
 	res, err := client.Do(req)
 	if err != nil {
-		return ErrAddReferrer
+		return fmt.Errorf("%s: %w", "error adding an account referrer", err)
 	}
-
 	res.Body.Close()
 
 	return nil
 }
 
 func (acc *Account) removeDevice(client *http.Client, cdata *ConfigData) error {
-	req, err := http.NewRequest("DELETE", cdata.BaseURL+"/reg/"+acc.ID, http.NoBody)
+	req, err := http.NewRequestWithContext(context.TODO(), http.MethodDelete, cdata.BaseURL+"/reg/"+acc.ID, http.NoBody)
 	if err != nil {
-		return ErrCreateRequest
+		return fmt.Errorf("%s: %w", "error creating a request to remove a device", err)
 	}
 
 	req = setCommonHeaders(cdata, req)
-
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	req.Header.Set("Authorization", "Bearer "+acc.Token)
 
 	res, err := client.Do(req)
 	if err != nil {
-		return ErrDeleteAccount
+		return fmt.Errorf("%s: %w", "error removing a device from account", err)
 	}
-
 	res.Body.Close()
 
 	return nil
 }
 
 func (acc *Account) setKey(client *http.Client, cdata *ConfigData, key string) error {
-	payload, _ := json.Marshal(map[string]string{
+	payload, err := json.Marshal(map[string]string{
 		"license": key,
 	})
-	req, err := http.NewRequest("PUT", cdata.BaseURL+"/reg/"+acc.ID+"/account", bytes.NewBuffer(payload))
-
-	req = setCommonHeaders(cdata, req)
-
 	if err != nil {
-		return ErrCreateRequest
+		return fmt.Errorf("%s: %w", "error marshalling account license", err)
+	}
+	req, err := http.NewRequestWithContext(context.TODO(), http.MethodPut,
+		cdata.BaseURL+"/reg/"+acc.ID+"/account", bytes.NewBuffer(payload))
+	if err != nil {
+		return fmt.Errorf("%s: %w", "error creating request with account key", err)
 	}
 
+	req = setCommonHeaders(cdata, req)
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	req.Header.Set("Authorization", "Bearer "+acc.Token)
 
 	res, err := client.Do(req)
 	if err != nil {
-		return ErrSetKey
+		return fmt.Errorf("%s: %w", "error setting account's key", err)
 	}
-
 	res.Body.Close()
 
 	return nil
 }
 
 func (acc *Account) fetchAccountData(client *http.Client, cdata *ConfigData) (*AccountData, error) {
-	req, err := http.NewRequest("GET", cdata.BaseURL+"/reg/"+acc.ID+"/account", http.NoBody)
+	req, err := http.NewRequestWithContext(context.TODO(), http.MethodGet,
+		cdata.BaseURL+"/reg/"+acc.ID+"/account", http.NoBody)
 	if err != nil {
-		return nil, ErrCreateRequest
+		return nil, fmt.Errorf("%s: %w", "error creating request to fetch account data", err)
 	}
 
 	req = setCommonHeaders(cdata, req)
-
 	req.Header.Set("Authorization", "Bearer "+acc.Token)
 
 	res, err := client.Do(req)
 	if err != nil {
-		return nil, ErrFetchAccData
+		return nil, fmt.Errorf("%s: %w", "error fetching account data", err)
 	}
 	defer res.Body.Close()
 
 	var accountData AccountData
 	if err := json.NewDecoder(res.Body).Decode(&accountData); err != nil {
-		return nil, ErrDecodeAccData
+		return nil, fmt.Errorf("%s: %w", "error decoding account data", err)
 	}
 
 	return &accountData, nil
