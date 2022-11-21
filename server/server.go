c2ef565a9f54a4b20f3c6e845a51c1b6e68e1b36
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-11-21 14:31:46 +0200

    server: update server to use new code

--- server/server.go
+++ server/server.go
@@ -1,11 +1,16 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
-	"gowarp/warp"
+	"github.com/handsomefox/gowarp/client"
+	"github.com/handsomefox/gowarp/client/cfg/pastebin"
+	"github.com/handsomefox/gowarp/client/cfg/serdar"
+	"github.com/handsomefox/gowarp/storage"
 )
 
 // Server is the main gowarp http.Handler.
@@ -13,24 +18,37 @@ type Server struct {
 	handler   http.Handler
 	templates *TemplateStorage
 
-	w *warp.Warp
+	client  *client.Client
+	storage *storage.Storage
 }
 
 func NewHandler() (*Server, error) {
 	// Create storage for templates
 	ts, err := NewTemplateStorage()
 	if err != nil {
-		return nil, fmt.Errorf("%s: %w", "error creating the server", err)
+		return nil, fmt.Errorf("error creating the server: %w", err)
 	}
-
-	// Create warp instance
-	wh := warp.New()
 
 	// Create server
 	server := &Server{
-		w:         wh,
 		templates: ts,
+		client:    client.NewClient(nil),
+		storage:   storage.NewStorage(),
 	}
+
+	go server.storage.Fill(server.client)
+	go func() {
+		for {
+			config, err := serdar.GetConfig(context.Background())
+			if err != nil {
+				time.Sleep(1 * time.Minute)
+			}
+			server.client.UpdateConfig(config)
+
+			time.Sleep(1 * time.Hour)
+		}
+	}()
+
 	mux := http.NewServeMux()
 	// Setup routes
 	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
@@ -69,9 +87,13 @@ func (s *Server) home() http.HandlerFunc {
 func (s *Server) updateConfig() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		message := "finished config update"
-		if err := s.w.UpdateConfig(r.Context()); err != nil {
+
+		newConfig, err := pastebin.GetConfig(r.Context())
+		if err != nil {
 			message = "failed to update config"
 		}
+		log.Println(newConfig)
+		s.client.UpdateConfig(newConfig)
 
 		if err := s.templates.Config().Execute(w, message); err != nil {
 			log.Println(err)
@@ -87,14 +109,14 @@ func (s *Server) generateKey() http.HandlerFunc {
 			return
 		}
 
-		generatedKey, err := s.w.GetKey(r.Context())
+		key, err := s.storage.GetKey(r.Context(), s.client)
 		if err != nil {
 			log.Println(err)
 			errorWithCode(w, http.StatusInternalServerError)
 			return
 		}
 
-		if err := s.templates.Key().Execute(w, generatedKey); err != nil {
+		if err := s.templates.Key().Execute(w, key); err != nil {
 			log.Println(err)
 			errorWithCode(w, http.StatusInternalServerError)
 		}
