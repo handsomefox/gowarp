005c267c17991ff233276804ca10277f79fb40c4
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-11-21 21:00:34 +0200

    storage: refactor, add docs

--- pkg/server/storage/storage.go
+++ pkg/server/storage/storage.go
@@ -12,62 +12,71 @@ import (
 	"github.com/handsomefox/gowarp/pkg/models"
 	"github.com/handsomefox/gowarp/pkg/models/mongo"
 	"github.com/handsomefox/gowarp/pkg/warp"
-	"github.com/handsomefox/gowarp/pkg/warp/keygen"
 )
 
 type Storage struct {
 	AM *mongo.AccountModel
 }
 
-// Fill fills the internal storage with correctly generated keys.
+// Fill fills (forever) the internal storage with correctly generated keys.
 func (store *Storage) Fill(s *warp.Service) {
 	for {
 		if store.AM.Len(context.Background()) > 250 {
 			time.Sleep(10 * time.Second)
 			continue
 		}
-		var wg errgroup.Group
-		var createdKey *models.Account
 
-		wg.Go(func() error {
-			key, err := keygen.MakeKey(context.Background(), s)
-			if err != nil {
-				return fmt.Errorf("error generating key: %w", err)
-			}
-			createdKey = key
+		store.makeKey(s)
 
-			return nil
-		})
-
-		if err := wg.Wait(); err != nil {
-			log.Printf("Error when generating key: %s", err)
-		} else {
-			i, err := createdKey.RefCount.Int64()
-			if err != nil {
-				log.Println("couldn't get generated key size")
-				continue
-			}
-			if i < 1000 {
-				log.Println("generated key was too small to use")
-				continue
-			}
-
-			id, err := store.AM.Insert(context.Background(), createdKey)
-			if err != nil {
-				log.Println("Failed to add key to the database", err)
-			}
-			log.Println("Added key to database, id: ", id)
-		}
-		log.Println("Currently stored keys: ", store.AM.Len(context.Background()))
+		log.Println("Currently storing: ", store.AM.Len(context.Background()), " keys")
 		time.Sleep(s.WaitTime())
 	}
+}
+
+// makeKey wraps the warp.MakeKey and stores the key inside database.
+func (store *Storage) makeKey(s *warp.Service) {
+	start := time.Now()
+	defer log.Println("Generating key took: ", time.Since(start).Milliseconds(), "ms")
+
+	var wg errgroup.Group
+	var createdKey *models.Account
+	wg.Go(func() error {
+		key, err := warp.MakeKey(context.Background(), s)
+		if err != nil {
+			return fmt.Errorf("error generating key: %w", err)
+		}
+		createdKey = key
+
+		return nil
+	})
+
+	if err := wg.Wait(); err != nil {
+		log.Println("Error when generating key: ", err)
+		return
+	}
+
+	i, err := createdKey.RefCount.Int64()
+	if err != nil {
+		log.Println("Couldn't get generated key size: ", err)
+		return
+	}
+	if i < 1000 {
+		log.Println("Generated key was too small to use: ", i)
+		return
+	}
+
+	id, err := store.AM.Insert(context.Background(), createdKey)
+	if err != nil {
+		log.Println("Failed to add key to the database: ", err)
+	}
+	log.Println("Added key to database, id: ", id)
 }
 
 // GetKey either returns a key that is already stored or creates a new one.
 func (store *Storage) GetKey(ctx context.Context, s *warp.Service) (*models.Account, error) {
 	item, err := store.AM.GetAny(ctx)
 	if err != nil {
-		key, err := keygen.MakeKey(ctx, s)
+		key, err := warp.MakeKey(ctx, s)
 		if err != nil {
 			return nil, fmt.Errorf("error generating key: %w", err)
 		}
@@ -77,6 +86,6 @@ func (store *Storage) GetKey(ctx context.Context, s *warp.Service) (*models.Acco
 		log.Println("Failed to remove key from database: ", err)
 	}
 
-	log.Println("Currently stored key size: ", store.AM.Len(ctx))
+	log.Println("Currently storing: ", store.AM.Len(ctx), " keys")
 	return item, nil
 }
