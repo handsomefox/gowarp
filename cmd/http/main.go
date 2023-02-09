923f419086daf813761aa57413909ee65aa722cf
Eduard Diachenko <handsomefox.hf@gmail.com>
2023-02-09 16:59:33 +0200

    cleanup: move main to cmd/http/

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"context"
 
-	"github.com/handsomefox/gowarp/server"
+	"github.com/handsomefox/gowarp/internal/server"
+	"github.com/handsomefox/gowarp/internal/server/templates"
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/sethvargo/go-envconfig"
 )
 
 type AppConfiguration struct {
-	DatabaseURI string `env:"DB_URI"`
-	Port        string `env:"PORT"`
+	DatabaseURI    string `env:"DB_URI"`
+	Port           string `env:"PORT"`
+	DatabaseName   string `env:"DATABASE_NAME"`
+	CollectionName string `env:"COLLECTION_NAME"`
 }
 
 func main() {
-	ctx := context.Background()
-
 	log.Level(zerolog.TraceLevel)
+
+	if err := godotenv.Load(); err != nil {
+		log.Err(err).Msg("failed to load .env file")
+	}
+
+	ctx := context.Background()
 
 	var c AppConfiguration
 	if err := envconfig.Process(ctx, &c); err != nil {
@@ -32,16 +40,17 @@ func main() {
 		c.Port = "8080"
 	}
 
-	tmpl, err := server.LoadTemplates()
+	tmpl, err := templates.Load()
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to load templates")
 	}
 
-	s, err := server.New(ctx, ":"+c.Port, c.DatabaseURI, tmpl)
+	s, err := server.New(ctx, ":"+c.Port, c.DatabaseURI, c.DatabaseName, c.CollectionName, tmpl)
 	if err != nil {
 		log.Fatal().Err(err).Send()
 	}
 
+	log.Info().Str("addr", "localhost").Str("port", c.Port).Msg("server started on http://localhost:" + c.Port)
 	if err := s.ListenAndServe(); err != nil {
 		log.Fatal().Err(err).Send()
 	}
